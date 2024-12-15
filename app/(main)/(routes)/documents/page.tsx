655762eb9326e518704808158fe1778344699a24
Amin Bin Hassan Aggag <z5584567@ad.unsw.edu.au>
2024-12-15 11:34:52 +1100

    feat: sidebar completed + log out button + database schema WIP

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+// without this "use client", there will be a SWR import error, alhamdulillah for this thread for the solution https://github.com/vercel/swr/issues/2694
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div>
-      This is a protected page
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/Trash_empty_light.svg"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/Trash_empty_dark.svg"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s IlmMind
+      </h2>
+      <Button>
+        <PlusCircleIcon className="h-4 w-4 mr-2"/>
+        Create a note
+      </Button>
     </div>
   );
 }
