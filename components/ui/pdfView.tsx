b5127a855df91a58887c55f7dc2eb19a9b99385f
Amin Bin Hassan Aggag <z5584567@ad.unsw.edu.au>
2025-02-22 21:31:58 +1100

    WIP: PDF viewing feature

--- components/ui/pdfView.tsx
+++ components/ui/pdfView.tsx
@@ -1,7 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import { Document, Page } from "react-pdf";
+import React from 'react';
+
+function PdfComp(props: any) {
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div className="pdf-div">
+           <p>
+        Page {pageNumber} of {numPages}
+      </p>
+      <Document file={props.pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.apply(null, Array(numPages))
+          .map((x, i) => i + 1)
+          .map((page) => {
+            return (
+              <Page
+                pageNumber={page}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+              />
+            );
+          })}
+      </Document>
+    </div>
+  );
+}
+export default PdfComp;
+
 export const PDFView = ({ className }: {className: string}) => {
+  // const pdfFile = useQuery()
+
   return (
     <div className={className}>
       PDFView
+      <PdfComp/>
     </div>
   )
 }
